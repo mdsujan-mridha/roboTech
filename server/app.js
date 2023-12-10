93f9caca676a1aa0e89f9e342d380c7a2d83c634
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-12-10 11:30:06 +0600

    create folder structure

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+
+
+
+
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,41 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+
+// handle uncaught Exception 
+process.on("uncaughtException",err =>{
+
+    console.log(`Err : ${err.message}`);
+  
+    console.log(` Shutting down the server due to uncaught Exception `);
+    
+    process.exit(1);
+  });
+
+// config 
+dotenv.config({ path: "./config/config.env" });
 
 
+const server = app.listen(port,() =>{
+    console.log(`Server is working on http://localhost:${port}`)
+  });
+  
+
+// default endpoint 
+app.get("/", async (req, res, next) => {
+    res.send("You RobotTech server is working");
+})
+
+// UnHandle Promise Rejection 
+process.on("unhandledRejection",err =>{
+
+    console.log(`Err : ${err.message}`);
+  
+    console.log(` Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close( () =>{
+       process.exit(1);
+    });
+  
+  });
 
