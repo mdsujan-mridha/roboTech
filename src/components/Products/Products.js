49c102d474c7c2393d2b268f852702508ace6e7e
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-12-18 01:53:29 +0600

    create product page without pagination

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,8 @@
 import { Search } from '@mui/icons-material';
 import { Box, Slider } from '@mui/material';
 import React, { useState } from 'react';
+import { products } from '../../utils/FakeData';
+import ProductCard from '../../utils/ProductCard';
 
 const categories = [
     "Robotic Toys",
@@ -14,13 +16,18 @@ const categories = [
 
 const Products = () => {
     const [value, setValue] = useState([20, 3000]);
+    const[rating,setRating] = useState([0,5]);
+
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-
+       
+    const handleRatingChange = (e,newValue)=>{
+        setRating(newValue)
+    }
     return (
         <>
-            <div className=' min-h-screen' style={{ backgroundColor: 'var(--secondary)' }}>
+            <div className='min-h-screen' style={{ backgroundColor: 'var(--secondary)' }}>
                 <div className='px-12 flex w-full pt-36'>
                     <input
                         type="text"
@@ -30,9 +37,10 @@ const Products = () => {
                         className='w-full h-14 bg-transparent border-2 rounded-full outline-none px-10 text-white font-bold text-sm' />
                     <button> <Search sx={{ color: 'orange', fontSize: '50px', marginLeft: '-70px' }} /></button>
                 </div>
-                <div className='flex justify-between items-center gap-5 mt-10 px-12'>
-                    <div className='w-full lg:w-1/5 h-screen rounded-md' style={{ backgroundColor: 'var(--primary)' }}>
-
+                {/* filter div */}
+                <div className='flex justify-between  gap-5 mt-10 px-12'>
+                    <div className='w-full lg:w-1/5 min-h-screen rounded-md pb-5' style={{ backgroundColor: 'var(--primary)' }}>
+                      {/* filter by category */}
                         <div className='pt-5 pb-2 px-5'>
                             <h1 className='text-white font-bold text-xl py-3'> Filter by Category </h1>
                             <hr style={{ color: 'gray', backgroundColor: 'gray', height: 2, border: 'none' }} />
@@ -47,13 +55,12 @@ const Products = () => {
                                 }
                             </div>
                         </div>
-
+                      {/* filter by price  */}
                         <div className='px-5'>
                             <h1 className='text-white font-bold text-xl py-3'> Filter by Price </h1>
                             <hr style={{ color: 'gray', backgroundColor: 'gray', height: 2, border: 'none' }} />
-                            <Box className="py-10">
+                            <Box className="py-5">
                                 <Slider
-                                    getAriaLabel={() => 'Temperature range'}
                                     value={value}
                                     onChange={handleChange}
                                     valueLabelDisplay="auto"
@@ -74,9 +81,63 @@ const Products = () => {
                                 />
                             </Box>
                         </div>
-
+                        {/* filter by brand  */}
+                        <div className='pt-2 pb-2 px-5'>
+                            <h1 className='text-white font-bold text-xl pb-3'> Filter by Brand  </h1>
+                            <hr style={{ color: 'gray', backgroundColor: 'gray', height: 2, border: 'none' }} />
+                            <div className='py-5 flex flex-col gap-3'>
+                                {
+                                    categories.map((category, index) => (
+                                        <li
+                                            className='list-none font-bold opacity-75 cursor-pointer hover:opacity-100'
+                                            key={index}
+                                        > {category} </li>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        {/* filter by rating  */}
+                        <div className='px-5'>
+                            <h1 className='text-white font-bold text-xl py-3'> Filter by Rating </h1>
+                            <hr style={{ color: 'gray', backgroundColor: 'gray', height: 2, border: 'none' }} />
+                            <Box className="py-5">
+                                <Slider
+                                    value={rating}
+                                    onChange={handleRatingChange}
+                                    valueLabelDisplay="auto"
+                                    aria-labelledby='range-slider'
+                                    min={0}
+                                    max={5}
+                                    sx={{
+                                        color: 'white', // Set the color to white
+                                        '& .MuiSlider-thumb': {
+                                            '&:hover, &.Mui-focusVisible': {
+                                                boxShadow: 'none', // Optional: Remove hover/focus shadow
+                                            },
+                                        },
+                                        '& .MuiSlider-rail': {
+                                            opacity: 0.5, // Adjust rail opacity if needed
+                                        },
+                                    }}
+                                />
+                            </Box>
+                        </div>
                     </div>
-                    <div className='w-full h-screen lg:w-4/5 rounded-md' style={{ backgroundColor: 'var(--primary)' }}></div>
+                    {/* product div  */}
+                    <div className='w-full min-h-screen lg:w-4/5 rounded-md pb-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-12 pt-5' style={{ backgroundColor: 'var(--primary)' }}>
+                        {
+                            products &&
+                            products.map((product) =>(
+                                <ProductCard
+                                key={product?._id}
+                                product={product}
+                                >
+
+                                </ProductCard>
+                            ))
+                            
+                        }
+                    </div>
                 </div>
             </div>
         </>
